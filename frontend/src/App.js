81d6a119a3ab2e9aee4b26ab505dab5c609d0f4f
Aleksey <bellatorjs1@gmail.com>
2022-12-23 12:32:47 +0300

    Added RollbarProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import leoProfanity from 'leo-profanity';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import Layout from './components/Layout';
 import LoginPage from './components/LoginPage';
 import NotFoundPage from './components/NotFoundPage';
@@ -18,32 +19,42 @@ leoProfanity.clearList();
 leoProfanity.add(leoProfanity.getDictionary('ru'));
 leoProfanity.add(leoProfanity.getDictionary('eng'));
 
-const App = () => (
+const rollbarConfig = {
+  accessToken: process.env.ACCESSTOKEN,
+  environment: 'production',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
 
-  <I18nextProvider>
-    <AuthProvider>
-      <SocketProvider>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route
-                index
-                element={(
-                  <PrivateRoute>
-                    <ChatPage />
-                  </PrivateRoute>
+const App = () => (
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      <I18nextProvider>
+        <AuthProvider>
+          <SocketProvider>
+            <div className="App">
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  <Route
+                    index
+                    element={(
+                      <PrivateRoute>
+                        <ChatPage />
+                      </PrivateRoute>
           )}
-              />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="signup" element={<SignUp />} />
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-          <ToastContainer />
-        </div>
-      </SocketProvider>
-    </AuthProvider>
-  </I18nextProvider>
+                  />
+                  <Route path="login" element={<LoginPage />} />
+                  <Route path="signup" element={<SignUp />} />
+                  <Route path="*" element={<NotFoundPage />} />
+                </Route>
+              </Routes>
+              <ToastContainer />
+            </div>
+          </SocketProvider>
+        </AuthProvider>
+      </I18nextProvider>
+    </ErrorBoundary>
+  </RollbarProvider>
 
 );
 
