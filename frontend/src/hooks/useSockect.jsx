5de4678e2cf259dee719fd98ae3fef9e81d27e54
Aleksey <bellatorjs1@gmail.com>
2022-12-30 20:30:15 +0300

    Commmit

--- frontend/src/hooks/useSockect.jsx
+++ frontend/src/hooks/useSockect.jsx
@@ -3,5 +3,5 @@ import { useContext } from 'react';
 import { SocketContext } from '../contexts/SocketContext';
 
 const useSocket = () => useContext(SocketContext);
-
+console.log("Yes")
 export default useSocket;
